84b6e7b3165a3928871da83e908143703d9a464f
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2015-08-17 13:23:22 +0200

    Extend view into titlebar

--- js/index.js
+++ js/index.js
@@ -112,9 +112,9 @@ WinJS.Namespace.define("Places", {
 
 if (typeof Windows !== 'undefined' && typeof Windows.UI !== 'undefined' &&  typeof Windows.UI.ViewManagement !== 'undefined') { 
   // Get a reference to the App Title Bar 
-   Windows.UI.ViewManagement.ApplicationView.getForCurrentView().title="Test title";
+   Windows.UI.ViewManagement.ApplicationView.getForCurrentView().title="";
      var appTitleBar = Windows.UI.ViewManagement.ApplicationView.getForCurrentView().titleBar; 
-
+appTitleBar.ExtendViewIntoTitleBar = true;
  
        appTitleBar.foregroundColor = {r:255,g:255,b:255,a:0} ;
      appTitleBar.backgroundColor =  {r:68,g:153,b:221,a:0} ;
